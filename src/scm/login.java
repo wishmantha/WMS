e54755782ed9a5cc9b27b9c824ef65fbb25fc8e8
Vihanga <vihanga.bandara@ifsworld.com>
2017-10-17 15:08:44 +0530

    sex

--- src/scm/login.java
+++ src/scm/login.java
@@ -202,7 +202,7 @@ private void txtPWActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:e
     private void loginAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginAdminActionPerformed
         if (clicked == false) {
             txtUN.setText("Admin");
-            txtPW.setText("vihanga123");
+            txtPW.setText("vihanga");
             loginWithConnection();
         }
     }//GEN-LAST:event_loginAdminActionPerformed
